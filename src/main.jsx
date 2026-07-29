ab75212c1d4f69a7be756d02eb74687c16cd3f18
alnrfLO <raf.atns1103@gmail.com>
2026-07-29 12:39:14 +0200

    Ajout Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext'
@@ -14,5 +15,6 @@ createRoot(document.getElementById('root')).render(
       </LanguageProvider>
     </BrowserRouter>
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 )
